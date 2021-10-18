ba1ea0d8ab59cc9acf23dfe3f339667692d79b84
PeterSparks-lab <71788421+PeterSparks-lab@users.noreply.github.com>
2021-10-18 10:57:14 -0600

    added player object

--- sketch.js
+++ sketch.js
@@ -1,15 +1,44 @@
-// Project Title
-// Your Name
-// Date
+// Major Project
+// Peter Sparks
+// Oct 18 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let man;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  man = new Player(width/2,height/2);
 }
 
 function draw() {
   background(220);
+  man.display();
+  // for (let i=0; i<10; i++) {
+  //   rect(50*i,10*i,50,100);
+  // }
+}
+
+function keyTyped() {
+  if (key === "w") {
+    man.y -= man.speed;
+  }
+  else if (key === "s") {
+    man.y += man.speed;
+  }
+}
+
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 15;
+    this.speed = 5;
+  }
+
+  display() {
+    fill("red");
+    rectMode(CENTER);
+    rect(this.x, this.y, this.size, this.size);
+  }
 }
